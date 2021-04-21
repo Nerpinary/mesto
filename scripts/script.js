77540b121f639594f7ab05c9f19cdfebc85e478a
nerpinary <nerpinary5693@gmail.com>
2021-04-22 00:04:05 +0800

    Практическая работа 4.1

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,17 @@
-let places = document.querySelector(".places");
-let likeButton = places.querySelectorAll(".place__like");
+let likeButton = document.querySelectorAll(".place__like");
+let profile = document.querySelector(".profile");
+let editButton = profile.querySelector(".profile__edit-button");
+let popup = document.querySelector(".popup");
+let closeButton = popup.querySelector(".popup__close-button");
+let inputName = popup.querySelector(".popup__input_data_name");
+let inputJob = popup.querySelector(".popup__input_data_job");
+let textName = profile.querySelector(".profile__name");
+let textJob = profile.querySelector(".profile__job");
+let save = popup.querySelector(".popup__form");
 
-for (let i=0; i < likeButton.length; i += 1) {
+
+for (let i = 0; i < likeButton.length; i += 1) {
 likeButton[i].addEventListener("click", function likeChange() {
-    
     if (likeButton[i].classList.contains("place__like_status_disabled") === true) {
         likeButton[i].classList.add("place__like_status_enabled");
         likeButton[i].classList.remove("place__like_status_disabled");
@@ -11,39 +19,30 @@ likeButton[i].addEventListener("click", function likeChange() {
         likeButton[i].classList.add("place__like_status_disabled");
         likeButton[i].classList.remove("place__like_status_enabled");
     }
-});
-}   
+})
+};   
 
- 
-let profile = document.querySelector(".profile");
-let editButton = profile.querySelector(".profile__edit-button");
-let popup = document.querySelector(".popup");
-let closeButton = popup.querySelector(".popup__close-button");
+editButton.addEventListener("click", openPopUp);
 
-editButton.addEventListener("click", function openPopUp() {
-    popup.classList.add("popup_opened");
-});
+closeButton.addEventListener("click", closePopUp);
 
-closeButton.addEventListener("click", function closePopUp(){
-    popup.classList.remove("popup_opened");
-});
+save.addEventListener("click", saveChanges);
 
-
-let inputName = popup.querySelector(".popup__input-name");
-let inputJob = popup.querySelector(".popup__input-job");
-let textName = profile.querySelector(".profile__name");
-let textJob = profile.querySelector(".profile__job");
-let saveButton = popup.querySelector(".popup__save-button");
-
-inputName.value = textName.textContent;
-inputJob.value = textJob.textContent;
-
-saveButton.addEventListener("click", function saveChanges() {
+function saveChanges() {
     textName.textContent = inputName.value;
     textJob.textContent = inputJob.value;
     popup.classList.remove("popup_opened");
-});
+};
 
+function openPopUp() {
+    popup.classList.add("popup_opened");
+    inputName.value = textName.textContent;
+    inputJob.value = textJob.textContent;
+};
+
+function closePopUp(){
+    popup.classList.remove("popup_opened");
+};
 
 
 
