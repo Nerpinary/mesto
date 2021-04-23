5cf49d3f9acdcb5566023f6cc7d25f9f923a0904
nerpinary <nerpinary5693@gmail.com>
2021-04-23 17:46:06 +0800

    Практическая работа 4.1

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,3 @@
-let likeButton = document.querySelectorAll(".place__like");
 let profile = document.querySelector(".profile");
 let editButton = profile.querySelector(".profile__edit-button");
 let popup = document.querySelector(".popup");
@@ -7,32 +6,7 @@ let inputName = popup.querySelector(".popup__input_data_name");
 let inputJob = popup.querySelector(".popup__input_data_job");
 let textName = profile.querySelector(".profile__name");
 let textJob = profile.querySelector(".profile__job");
-let save = popup.querySelector(".popup__form");
-
-
-for (let i = 0; i < likeButton.length; i += 1) {
-likeButton[i].addEventListener("click", function likeChange() {
-    if (likeButton[i].classList.contains("place__like_status_disabled") === true) {
-        likeButton[i].classList.add("place__like_status_enabled");
-        likeButton[i].classList.remove("place__like_status_disabled");
-    } else {
-        likeButton[i].classList.add("place__like_status_disabled");
-        likeButton[i].classList.remove("place__like_status_enabled");
-    }
-})
-};   
-
-editButton.addEventListener("click", openPopUp);
-
-closeButton.addEventListener("click", closePopUp);
-
-save.addEventListener("submit", saveChanges);
-
-function saveChanges() {
-    textName.textContent = inputName.value;
-    textJob.textContent = inputJob.value;
-    popup.classList.remove("popup_opened");
-};
+let formElement = popup.querySelector(".popup__form");
 
 function openPopUp() {
     popup.classList.add("popup_opened");
@@ -44,5 +18,17 @@ function closePopUp(){
     popup.classList.remove("popup_opened");
 };
 
+function saveChanges(event) {
+    event.preventDefault();
+    textName.textContent = inputName.value;
+    textJob.textContent = inputJob.value;
+    closePopUp();
+};
+
+editButton.addEventListener("click", openPopUp);
+
+closeButton.addEventListener("click", closePopUp);
+
+formElement.addEventListener("submit", saveChanges);
 
 
