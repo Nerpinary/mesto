375ee573c391c54869b51b8651de67c267a38853
Nerpinary <nerpinary5693@gmail.com>
2021-06-04 01:32:53 +0300

    Практическая работа 7

--- scripts/index.js
+++ scripts/index.js
@@ -5,20 +5,15 @@ const editButton = profile.querySelector(".profile__edit-button");
 const addButton = document.querySelector(".profile__add-button");
 const popupEdit = document.querySelector("#popupEdit");
 const popupAdd = document.querySelector("#popupAdd");
-const popupImage = document.querySelector("#popupImage");
-const imagePopup = document.querySelector(".popup__image");
-const captionPopup = document.querySelector(".popup__image-caption");
 const inputName = document.querySelector(".popup__input_data_name");
 const inputJob = document.querySelector(".popup__input_data_job");
 const inputPlace = document.querySelector(".popup__input_data_place");
 const inputLink = document.querySelector(".popup__input_data_link");
 const textName = profile.querySelector(".profile__name");
 const textJob = profile.querySelector(".profile__job");
-const textPlace = document.querySelector("place__name");
 const formElementEdit = document.querySelector("#formElementEdit");
 const formElementAdd = document.querySelector("#formElementAdd");
 const cardsList = document.querySelector(".places__list");
-const cardsTemplate = document.querySelector(".place-template").content;
 const validate = new FormValidator(config);
 
 function addCard(data, wrap) {
@@ -34,24 +29,19 @@ initialCards.forEach((item) => {
 });
 
 function profileFormSubmitHandler(event) {
-    
     event.preventDefault();
     textName.textContent = inputName.value;
     textJob.textContent = inputJob.value;
-
     closePopup(popupEdit);
 };
 
 function cardFormSubmitHandler(event) {
     event.preventDefault();
-
     addCard({
         name: inputPlace.value,
         link: inputLink.value
     }, cardsList);
-    
     formElementAdd.reset();
-    
     closePopup(popupAdd);
 };
 
@@ -62,7 +52,6 @@ export function openPopup(popup) {
 };
 
 function openPopupEdit() {
-    
     inputName.value = textName.textContent;
     inputJob.value = textJob.textContent;
     validate.enableValidation(formElementEdit);
